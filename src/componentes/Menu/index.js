d449348bbf14003d0b8d71a941ba92d0ae1d35ec
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2026-03-25 18:29:18 -0300

    ajuste no cabeçalho

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,16 +1,28 @@
 import styles from './Menu.module.css'; 
+import { Link } from "react-router-dom";
 import MenuLink from '../MenuLink';
 
 export default function Menu () {
     return (
-        <header>
+        <header className={styles.cabecalho}>
             <nav className={styles.navegacao}>
-                <MenuLink className={styles.link} to="/">
-                    Inicio
-                </MenuLink>
-                <MenuLink className={styles.link} to="/sobremim">
-                    Sobre Mim
-                </MenuLink>
+                <Link className={styles.marca} to="/">
+                    <span className={styles.marcaIcone}>BL</span>
+
+                    <span className={styles.marcaTexto}>
+                        <strong>Bruno Loureiro</strong>
+                        <span>Portfólio Full Stack</span>
+                    </span>
+                </Link>
+
+                <div className={styles.links}>
+                    <MenuLink to="/">
+                        Início
+                    </MenuLink>
+                    <MenuLink to="/sobremim">
+                        Sobre Mim
+                    </MenuLink>
+                </div>
             </nav>
         </header>
     )
